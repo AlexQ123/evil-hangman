6b603dc0818b41ff33c9c5d00b16cc2e8c9d119c
Alex Qian <qianalex8@gmail.com>
2022-01-18 19:24:57 -0700

    inital completion, yet to be passed off

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,9 +1,154 @@
 package hangman;
 
+import java.io.*;
+import java.util.*;
+
 public class EvilHangman {
 
     public static void main(String[] args) {
 
+        // error checking command line parameters
+        String dictionaryFileName = "";
+        int wordLength = 0;
+        int numGuesses = 0;
+
+        // user must provide 3 arguments
+        if (args.length == 3) {
+            // dictionary error checking done later
+            dictionaryFileName = args[0];
+            // make sure user enters integers
+            try {
+                wordLength = Integer.parseInt(args[1]);
+            }
+            catch (NumberFormatException e) {
+                System.err.println("Please enter an integer word length.");
+            }
+            try {
+                numGuesses = Integer.parseInt(args[2]);
+            }
+            catch (NumberFormatException e) {
+                System.err.println("Please enter an integer guess number.");
+            }
+        }
+        else {
+            System.err.println("Please enter the proper number of arguments.");
+        }
+
+        // wordLength must be >= 2, numGuesses must be >= 1
+        if (wordLength < 2) {
+            System.err.println("Please enter a word length of 2 or greater.");
+        }
+
+        if (numGuesses < 1) {
+            System.err.println("Please enter a guess number of 1 or greater.");
+        }
+
+        // after error checking command line arguments, try to start game
+        File file = new File(dictionaryFileName);
+        IEvilHangmanGame game = new EvilHangmanGame();
+
+        try {
+            game.startGame(file, wordLength);
+        }
+        catch (IOException e) {
+            System.err.println("There was an error reading in your file.");
+        }
+        catch (EmptyDictionaryException e) {
+            System.err.println("The dictionary does not contain any words of the provided word length.");
+        }
+
+        // if the game successfully starts
+        // initialize some values needed for output
+        Set<String> possibleWords = new HashSet<>();
+        char guess = 0;
+        String userInput = "";
+        StringBuilder wordSoFar = new StringBuilder();
+        for (int i = 0; i < wordLength; i++) {
+            wordSoFar.append("-");
+        }
+
+        while (numGuesses > 0) {
+
+            // output number of remaining guesses and guessed letters
+            System.out.println("You have " + numGuesses + " guesses left");
+            System.out.print("Used letters:");
+            SortedSet<Character> guessedLetters = game.getGuessedLetters();
+            for (char letter : guessedLetters) {
+                System.out.print(" " + letter);
+            }
+            System.out.print("\n");
+
+            // output the word they've guessed so far (if it's the first time, it will output all '-')
+            System.out.print("Word: " + wordSoFar + "\n");
+
+            // read in user's guess
+            System.out.print("Enter guess: ");
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.nextLine();
+            // if the user enters more than one character, reject
+            while (userInput.length() != 1) {
+                System.out.println("Invalid input! Enter guess: ");
+                userInput = scanner.nextLine();
+            }
+            // if the character is not an upper or lower case letter, reject
+            while (!Character.isLetter(userInput.charAt(0)) || Character.isWhitespace(userInput.charAt(0))) {
+                System.out.print("Invalid input! Enter guess: ");
+                userInput = scanner.nextLine();
+            }
+
+            // now that you know the input is valid, make the guess
+            guess = Character.toLowerCase(userInput.charAt(0));
+            try {
+                possibleWords = game.makeGuess(guess);
+            }
+            catch (GuessAlreadyMadeException e) {
+                System.err.println("Oops! You already guessed that letter!");
+            }
+
+            // now that you have the possible words remaining, if the words don't have the user's guess
+            String inPossibleWords = possibleWords.iterator().next();
+            if (!inPossibleWords.contains(Character.toString(guess))) {
+                System.out.println("Sorry, there are no " + guess + "'s\n\n");
+                numGuesses--;
+            }
+            // else, if the word does contain the user's guess
+            else {
+                int charCount = 0;
+                for (int i = 0; i < inPossibleWords.length(); i++) {
+                    if (inPossibleWords.charAt(i) == guess) {
+                        charCount++;
+                    }
+                }
+                if (charCount > 1) {
+                    System.out.println("Yes, there are " + charCount + " " + guess + "'s\n\n");
+                }
+                else {
+                    System.out.println("Yes, there is " + charCount + " " + guess + "\n\n");
+                }
+            }
+
+            // update wordSoFar if necessary
+            for (int i = 0; i < inPossibleWords.length(); i++) {
+                if (inPossibleWords.charAt(i) == guess) {
+                    wordSoFar.replace(i, i+1, Character.toString(guess));
+                }
+            }
+
+            // if wordSoFar has no more '-', the user has won
+            if (!wordSoFar.toString().contains("-")) {
+                System.out.println("You win! You guessed the word: " + wordSoFar);
+                break;
+            }
+
+            // once numGuesses reaches zero, the user loses
+            if (numGuesses == 0) {
+                System.out.println("You lose!");
+                System.out.println("The word was: " + inPossibleWords);
+            }
+
+        }
+
     }
+
 
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -2,22 +2,181 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+
+    private Set<String> wordSet;
+    private SortedSet<Character> guessed;
+    private Map<String, Set<String>> partition;
+
+    public EvilHangmanGame() {
+        guessed = new TreeSet<>();
+        wordSet = new HashSet<>();
+        partition = new HashMap<>();
+    }
+
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+
+        // clear wordSet, guessed, and partition
+        wordSet.clear();
+        guessed.clear();
+        partition.clear();
+
+        // if the file doesn't exist, throw exception
+        if (!dictionary.exists()) {
+            throw new IOException();
+        }
+
+        // scan in words
+        Scanner scanner = new Scanner(dictionary);
+        while (scanner.hasNext()) {
+            String in = scanner.next();
+            // we're only interested if the length matches wordLength
+            if (in.length() == wordLength) {
+                // if there is a bad character in the word, throw exception
+                for (int i = 0; i < in.length(); i++) {
+                    char toCheck = in.charAt(i);
+                    if (!Character.isLetter(toCheck)) {
+                        throw new IOException();
+                    }
+                }
+                // otherwise, add the word into wordSet
+                String toAdd = in.toLowerCase();
+                wordSet.add(toAdd);
+            }
+        }
+
+        // if wordSet is empty, throw EmptyDictionaryException
+        if (wordSet.isEmpty()) {
+            throw new EmptyDictionaryException();
+        }
 
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+
+        // case insensitive
+        guess = Character.toLowerCase(guess);
+
+        // if the character has already been guessed, throw exception
+        if (guessed.contains(guess)) {
+            throw new GuessAlreadyMadeException();
+        }
+
+        // if not, add it to the set of guessed characters
+        guessed.add(guess);
+
+        // clear partition, then partition the wordSet
+        partition.clear();
+        partition(guess);
+
+        // set the current biggest set to the first set in partition
+        Set<String> resultSet = partition.values().iterator().next();
+        // for each set in partition, check to see if it has a larger size and make that the new resultSet if it does
+        for (Set<String> toCheck : partition.values()) {
+            if (toCheck.size() > resultSet.size()) {
+                resultSet = toCheck;
+            }
+            // if the two sets are the same size, choose the one in which the letter does not appear at all
+            else if (toCheck.size() == resultSet.size()) {
+
+                // does the guessed letter appear in a word from resultSet? if so, how many times?
+                boolean appearsInResult = false;
+                int resultCount = 0;
+                String resultWord = resultSet.iterator().next();
+                for (int i = 0; i < resultWord.length(); i++) {
+                    if (resultWord.charAt(i) == guess) {
+                        appearsInResult = true;
+                        resultCount++;
+                    }
+                }
+
+                // does the guessed letter appear in a word from toCheck? if so, how many times?
+                boolean appearsInCheck = false;
+                int checkCount = 0;
+                String checkWord = toCheck.iterator().next();
+                for (int i = 0; i < checkWord.length(); i++) {
+                    if (checkWord.charAt(i) == guess) {
+                        appearsInCheck = true;
+                        checkCount++;
+                    }
+                }
+
+                if (!appearsInCheck) {
+                    resultSet = toCheck;
+                }
+                // if both have the guessed letter, choose the one with the fewest letters
+                else if (appearsInResult && appearsInCheck) {
+                    if (checkCount < resultCount) {
+                        resultSet = toCheck;
+                    }
+                    /* if they both have the letter the same amount of times, choose the one with the rightmost guessed
+                    letter. if their first instance of the guessed letter is at the same spot, check the next instance
+                    of the guessed letter in both words, choosing the one that is rightmost */
+                    else if (checkCount == resultCount) {
+                        ArrayList<Integer> resultIndices = findIndices(resultWord, guess);
+                        ArrayList<Integer> checkIndices = findIndices(checkWord, guess);
+                        for (int i = 0; i < resultIndices.size(); i++) {
+                            if (checkIndices.get(i) > resultIndices.get(i)) {
+                                resultSet = toCheck;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        wordSet = resultSet;
+        return resultSet;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessed;
+    }
+
+    public void partition(char letter) {
+        // for each word in wordSet, calculate its SubsetKey and add entry to map
+        for (String word : wordSet) {
+            String subsetKey = getSubsetKey(word, letter);
+            /* if the map already has this key entry, copy the existing set, add the word into it,
+            and replace the existing set with this new set */
+            if (partition.containsKey(subsetKey)) {
+                Set<String> toInsert = partition.get(subsetKey);
+                toInsert.add(word);
+                partition.put(subsetKey, toInsert);
+            }
+            else {
+                Set<String> toInsert = new HashSet<>();
+                toInsert.add(word);
+                partition.put(subsetKey, toInsert);
+            }
+        }
+    }
+
+    public String getSubsetKey(String word, char letter) {
+        StringBuilder subsetKey = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                subsetKey.append(letter);
+            }
+            else {
+                subsetKey.append("-");
+            }
+        }
+        return subsetKey.toString();
+    }
+
+    public ArrayList<Integer> findIndices(String word, char letter) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
